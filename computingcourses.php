c6e338b36d69acb774e9837b9761243c8f149502
JAMES BRIGGS <c2094665@hallam.shu.ac.uk>
2024-01-01 16:46:36 +0000

    Computing

--- computingcourses.php
+++ computingcourses.php
@@ -22,7 +22,17 @@ include("header.php");
 ?>
 
 <div class="container">
-   <img class = "containerImage" src="images/Computingcoursepic.jpg"></div> 
+   <img class = "containerImage" src="images/Computingcoursepic.jpg">
+</div> 
+<div class = "paragraphComputerCourses">
+        <p>The College offers a range of courses to suit applicants with varying backgrounds and educational abilities. At undergraduate level, there are single BSc Honours Degree courses in Computing, Computer Networks, Software Engineering and Cyber Security with Forensics amongst others.</p>
+        <p>The College teaches undergraduate and postgraduate courses in a wide range of specialisms, including computer science, software development, information systems, networking and software engineering.</p>
+       <p> It is at the heart of a passionate computing community, including student societies devoted to games development, digital forensics and programming.</P>
+       <p> The courses are British Computer Society accredited and are highly relevant to modern industry. They are designed to prepare students for professional occupations in Computing and related fields.</p>
+       <p> Many graduates continue their studies to pursue a higher degree such as an MSc. or PhD </p>
+
+
+  </div>
 
 
 
