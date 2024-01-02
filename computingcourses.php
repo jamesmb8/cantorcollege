6585299de0d21625ad0f1d0207572a3b72d8670d
JAMES BRIGGS <c2094665@hallam.shu.ac.uk>
2024-01-02 12:08:08 +0000

    pages

--- computingcourses.php
+++ computingcourses.php
@@ -22,9 +22,9 @@ include("header.php");
 ?>
 
 <div class="container">
-   <img class = "containerImage" src="images/Computingcoursepic.jpg">
+   <img class = "containerImage" src="images/Computingcoursepic.jpg" >
 </div> 
-<div class = "paragraphComputerCourses">
+<div class = "paragraph" >
         <p>The College offers a range of courses to suit applicants with varying backgrounds and educational abilities. At undergraduate level, there are single BSc Honours Degree courses in Computing, Computer Networks, Software Engineering and Cyber Security with Forensics amongst others.</p>
         <p>The College teaches undergraduate and postgraduate courses in a wide range of specialisms, including computer science, software development, information systems, networking and software engineering.</p>
        <p> It is at the heart of a passionate computing community, including student societies devoted to games development, digital forensics and programming.</P>
@@ -33,9 +33,27 @@ include("header.php");
 
 
   </div>
+  
+  <div class="container">
+   <img class = "containerImage" src="images/ACES-032-Computing-Playstation-Lab.jpg" >
+</div> 
+
+<div class = "paragraph" >
+  <p> The College teaches undergraduate and postgraduate courses in a wide range of specialisms, including 
+computer science, software development, information systems, networking and software engineering. <p>
+<p>It is at the heart of a passionate computing community, including student societies devoted to games development, digital forensics and programming.<p>
+<p> The courses are British Computer Society accredited and are highly relevant to modern industry. <p>
+ <p> They are designed to prepare students for professional occupations in Computing and related fields. <p>
+  <p> Many graduates continue their studies to pursue a higher degree such as an MSc. or PhD.<p>
 
 
 
+</div>
+
+
+<?php
+include("footer.php");
+?>
 
 
 </body>
--- designcourses.php
+++ designcourses.php
@@ -21,5 +21,38 @@
 include("header.php");
 ?>
     
+    <div class="container">
+   <img class = "containerImage" src="images/IMG_0170.jpeg" >
+</div> 
+
+<div class = "paragraph" >
+<p>The College is an internationally connected creative community of diverse disciplines housed under one roof. <p>
+<p>We shape our students' futures, preparing them to shape the world through applied knowledge and creativity <p>
+<p>The College's art and design courses don't just train you, they promote alternative ways of thinking, making and communicating; they provide you with space, tools and inspiration to develop your creative practice and a clear career path. <p>
+<p>You'll get expert teaching from active practitioners and researchers who will encourage you to adopt innovative and resourceful approaches that both perceive and create opportunities for better lives.<p>
+</div>
+
+<div class = "paragraph" >
+<p>You will develop your creative practice whilst interacting with your peers in well-equipped studios, making and digital workshops.<p>
+<p>At the same time, you'll learn professional skills by working on applied briefs facilitated through our links with commercial clients, cultural institutions, businesses and organisations.<p>
+</div>
+
+<?php
+include("footer.php");
+?>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
 </body>
 </html>
